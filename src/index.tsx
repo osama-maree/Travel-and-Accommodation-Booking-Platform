a2112a844b0e1ede73346e9c9f121beeef4878fc
osama maree <osamamarei2001@gmail.com>
2024-01-11 21:29:38 +0200

    Upload Base Project

---
+++ src/index.tsx
@@ -0,0 +1,34 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import SnackbarProvider from "./context/SnackbarContext";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./queryCient";
+import MUIThemeProvider from "./styling/MUIThemeProvider";
+import { persistor, store } from "./store";
+const root = ReactDOM.createRoot(
+  document.getElementById("root") as HTMLElement
+);
+root.render(
+  <React.StrictMode>
+    <Router>
+      <Provider store={store}>
+        <PersistGate loading={"loading"} persistor={persistor}>
+          <MUIThemeProvider>
+            <QueryClientProvider client={queryClient}>
+              <SnackbarProvider>
+                <Routes>
+                  <Route path="/*" element={<App />} />
+                </Routes>
+              </SnackbarProvider>
+            </QueryClientProvider>
+          </MUIThemeProvider>
+        </PersistGate>
+      </Provider>
+    </Router>
+  </React.StrictMode>
+);
