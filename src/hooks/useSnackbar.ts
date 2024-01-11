3754cfb28b86e5f6b012886493cf30f23dcaffd7
osama maree <osamamarei2001@gmail.com>
2024-01-11 22:40:43 +0200

    Add SnackBar

---
+++ src/hooks/useSnackbar.ts
@@ -0,0 +1,14 @@
+import {useContext} from "react";
+import {SnackbarContext} from "../context/SnackbarContext";
+
+
+
+const useSnackbar = () => {
+    if (!SnackbarContext) {
+        throw new Error("useSnackbar must be used within the StoreProvider");
+    }
+
+    return useContext(SnackbarContext)
+};
+
+export default useSnackbar;
