eaa3016944d5033aee5c655ced23c5a9bd777d86
osama maree <osamamarei2001@gmail.com>
2024-01-18 12:34:19 +0200

    Deboune Hooks

---
+++ src/hooks/useDebounse.ts
@@ -0,0 +1,19 @@
+import { useEffect, useState } from "react";
+import { dataTypes } from "../pages/User/types";
+
+const useDebounce = (value:dataTypes | null, delay = 500) => {
+  const [debouncedValue, setDebouncedValue] = useState<dataTypes | null>(value);
+
+  useEffect(() => {
+    const id = setTimeout(() => {
+      setDebouncedValue(value);
+    }, delay);
+
+    return () => {
+      clearTimeout(id);
+    };
+  }, [value, delay]);
+
+  return debouncedValue;
+};
+export default useDebounce;
